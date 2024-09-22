026eb22cc03867828bc7bd3073bbd5dbbd9efeed
S Raghav <ragsvds710@gmail.com>
2024-09-22 14:49:29 +0530

    Update history

--- calculator_service/calculator.py
+++ calculator_service/calculator.py
@@ -4,18 +4,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app, origins="http://localhost:5003")
-
-@app.route('/calculate', methods=['POST'])
-@cross_origin()
-def calculate_result():
-    data = request.get_json()
-    operator = data['operator']
-    num1 = data['num1']
-    num2 = data['num2']
-
-    result = calculate(operator, num1, num2)
-    return jsonify({'result': result})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
@@ -45,3 +34,16 @@ def calculate(operator, num1, num2):
         return num1 / num2
     else:
         raise ValueError("Invalid operator.")
+
+@app.route('/calculate', methods=['POST', 'OPTIONS', 'GET'])
+@cross_origin()
+def calculate_result():
+    data = request.get_json()
+    operator = data['operator']
+    num1 = data['num1']
+    num2 = data['num2']
+
+    result = calculate(operator, num1, num2)
+    response = jsonify({'result': result})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
